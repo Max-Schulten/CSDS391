855f351a2fbd7b7b6cbbe745a7a4b0608b3bf12d
Max Schulten <maximilianschulten1@gmail.com>
2024-09-08 19:43:03 -0400

    Adding BFS and DFS to find solutions

--- assignment2_mls384/puzzle.py
+++ assignment2_mls384/puzzle.py
@@ -1,5 +1,43 @@
 import sys
 import random
+import re
+import copy
+
+
+# Defining a class for a node to use for DFS and BFS
+class Node:
+    # Constructor
+    def __init__(self, parent=None, direction=None, state=None):
+        # Using optional parameters because root node will not have parent or directoin but will have a state argument
+        if parent and direction and not state:
+            # Trivial Constrcutor Tasks
+            self.parent = parent
+            self.direction = direction
+            # Create current state of this node based on parents state and the direction of the move we are moving
+            arr = move(direction, state=copy.deepcopy(parent.state))
+            self.state = arr
+            self.zero = findZero(self.state)
+        # If a state is provided it will be the root node
+        elif state:
+            # Setting these fields for semantics
+            self.parent = None
+            self.direction = None
+            self.state = state
+            # Finding the zero in the given state
+            self.zero = findZero(state=state)
+        # Defining an array for each direction we move
+        arr = ['left', 'right', 'up', 'down']
+        # Creating the list to store children of node
+        children = []
+        for dir in arr:
+            if checkMove(dir, self.zero[0], self.zero[1]):
+                children.append(dir)
+        # Storing valid directions for children
+        self.children = children
+
+    # Adding this string representation for debugging purposes
+    def __str__(self):
+        return (f'\n{self.state}\n')
 
 # Define the goal state for the 8-puzzle as a 3x3 grid.
 goal_state = [
@@ -14,10 +52,6 @@ current_state = [
     [3, 4, 5],
     [6, 7, 8]
 ]
-
-
-# Initializing a nodes created counter
-nodes_created = 0
 
 
 # Main method that runs when the script is executed.
@@ -73,6 +107,15 @@ def executeCommand(cmd, line):
             move(arr[0])
         elif cmd == "scrambleState":
             scrambleState(arr[0])
+        elif cmd == "solve":
+            if len(arr) == 2:
+                maxnodes = int((re.search(r'\d+', arr[1])).group())
+            else:
+                maxnodes = -1
+            if arr[0] == "DFS":
+                dfs(maxnodes)
+            elif arr[0] == "BFS":
+                bfs(maxnodes)
         else:
             # Print an error message for invalid commands.
             print(f"Error: invalid command at line {line}")
@@ -174,6 +217,8 @@ def move(direction, state=None):
         elif direction == "right":
             state[i][j], state[i][j+1] = state[i][j+1], 0
 
+        return state
+
 
 # Helper method to check if a move in a given direction is possible.
 def checkMove(direction, i, j):
@@ -192,17 +237,110 @@ def checkMove(direction, i, j):
 
 # Helper method to find the position of the empty space (zero) in the current state.
 def findZero(state=None):
+    # Using an optional parameter to change which state we are editing
     if state is None:
         for i in range(3):
             for j in range(3):
                 if current_state[i][j] == 0:
                     return [i, j]
     else:
-        if state is None:
-            for i in range(3):
-                for j in range(3):
-                    if state[i][j] == 0:
-                        return [i, j]
+        for i in range(3):
+            for j in range(3):
+                if state[i][j] == 0:
+                    return [i, j]
+
+
+def dfs(maxnodes):
+    if maxnodes == -1:
+        maxnodes = 1000
+
+    root = Node(state=current_state)
+
+    # Defining counter to limit maxnodes
+    counter = 0
+
+    # Defining the queue for BFS
+    stack = [root]
+
+    # Defining an array with the solution steps as elements
+    solution = None
+
+    # Actual BFS search implementation
+    while stack and counter < maxnodes:
+        node = stack.pop()
+        if node.state == goal_state:
+            solution = backtrack(node)
+            break
+        else:
+            for child in node.children:
+                newChild = Node(parent=node, direction=child)
+                stack.append(newChild)
+                counter += 1
+    # Check all remaining created but unvisited nodes
+    for node in stack:
+        if node.state == goal_state:
+            solution = backtrack(node)
+
+    # Checking if a solution has been found
+    if not solution:
+        print(f"Error: maxnodes limit ({maxnodes}) reached")
+    else:
+        string = f"Nodes created during search: {counter}\nSolution length = {len(solution)}\nMove sequence:\n"
+        for move in solution:
+            if move:
+                string += f"move {move}\n"
+        print(string)
+
+
+
+def bfs(maxnodes):
+    if maxnodes == -1:
+        maxnodes = 1000
+
+    root = Node(state=current_state)
+
+    # Defining counter to limit maxnodes
+    counter = 0
+
+    # Defining the queue for BFS
+    queue = [root]
+
+    # Defining an array with the solution steps as elements
+    solution = None
+
+    # Actual BFS search implementation
+    while queue and counter < maxnodes:
+        node = queue.pop(0)
+        if node.state == goal_state:
+            solution = backtrack(node)
+            break
+        else:
+            for child in node.children:
+                newChild = Node(parent=node, direction=child)
+                queue.append(newChild)
+                counter += 1
+
+    for node in queue:
+        if node.state == goal_state:
+            solution = backtrack(node)
+
+    if not solution:
+        print(f"Error: maxnodes limit ({maxnodes}) reached")
+    else:
+        string = f"Nodes created during search: {counter}\nSolution length = {len(solution)}\nMove sequence:\n"
+        for move in solution:
+            if move:
+                string += f"move {move}\n"
+        print(string)
+
+
+def backtrack(node):
+    solution = [node.direction]
+    parent = node.parent
+    while parent is not None:
+        solution.append(parent.direction)
+        parent = parent.parent
+    return solution[::-1]
 
 
 # Standard Python convention to run the main method when the script is executed directly.
@@ -210,45 +348,3 @@ if __name__ == "__main__":
     main()
 
 
-# Defining a class for a node to use for DFS and BFS
-class Node:
-    # Constructor
-    def __init__(self, parent=None, direction=None, state=None):
-        # Using optional parameters because root node will not have parent or directoin but will have a state argument
-        if parent and direction and not state:
-            # Trivial Constrcutor Tasks
-            self.parent = parent
-            self.direction = direction
-            # Create current state of this node based on parents state and the direction of the move we are moving
-            arr = parent.state
-            arr = move(direction, state=parent.state)
-            self.state = arr
-            # Keeping track of the zero so we only need to call the find zero function once, keeping track on the fly here
-            zero = parent.zero
-            if (direction == 'up'):
-                zero[0] -= 1
-            if (direction == 'down'):
-                zero[0] += 1
-            if (direction == 'left'):
-                zero[1] -= 1
-            if (direction == 'right'):
-                zero[1] += 1
-            self.zero = zero
-        # If a state is provided it will be the root node
-        elif state:
-            # Setting these fields for semantics
-            self.parent = None
-            self.direction = None
-            self.state = state
-            # Finding the zero in the given state
-            self.zero = findZero(state=state)
-        # Defining an array for each direction we move
-        arr = ['left', 'right', 'up', 'down']
-        # Creating a temporary counter to track how many nodes we create
-        nodes = 0
-        # Creating the list to store children of node
-        children = []
-        for dir in arr:
-            if checkMove(dir, self.zero[0], self.zero[1]):
-                children.append(Node(parent=self, direction=dir))
-                nodes += 1
